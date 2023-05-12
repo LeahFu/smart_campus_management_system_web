642e7745386636ef9850159e239e3c4a4168438e
LeahFu <121897528+LeahFu@users.noreply.github.com>
2023-05-12 09:37:27 -0400

    import pinia & element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import App from "./App.vue"
 import router from "./router";
+import pinia from "./store";
+import ElementPlus from "element-plus"
+import "element-plus/dist/index.css"
+
+//Import all element-plus icon
+import * as ElementPlusIconsVue from "@element-plus/icons-vue"
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
+
+//Register all icons globally
+for(const [key, component] of Object.entries(ElementPlusIconsVue)){
+    app.component(key, component)
+}
+app.use(ElementPlus)
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 //1. import Vue Router module
 import { createRouter, createWebHashHistory } from "vue-router"
-// Define some routes, each route needs to be mapped to a component
+//2. Define some routes, each route needs to be mapped to a component
 const routes = [{
     path:'/',
     name:'Login',
     meta:{title:'smart campus management system login'},
     component:()=>import('../views/Login.vue')
 }]
-//
+//3. Create routing instance and pass routes configuration
 const router = createRouter({
     history: createWebHashHistory(),
     routes:routes
